c563682db3a4ae2612ff3e913930aec733c5d6ad
Kovalyov <dev.kovalyov@gmail.com>
2025-12-05 17:17:51 +0300

    added business logic to hotel info

--- Hotel/views.py
+++ Hotel/views.py
@@ -1,13 +1,17 @@
 from django import http
 from django.shortcuts import render
 from Hotel.forms import CreateHotel
-from Hotel.models import Hotel
+from Hotel.models import Hotel, Room
 
 
 def index(request):
     hotels = Hotel.objects.all()
     return render(request, 'index.html', {'hotels': hotels})
 
+def hotel_info(request, hotel_id):
+    hotel = Hotel.objects.get(id=hotel_id)
+    rooms = Room.objects.filter(hotel=hotel)
+    return render (request, 'hotel_info', {'hotel': hotel, 'rooms': rooms})
 
 
 
