bdf86387d1da2ec342c1c3eeb056f53a2201981b
Kovalyov <dev.kovalyov@gmail.com>
2025-12-05 13:33:55 +0300

    installed postgres

--- Diploma/settings.py
+++ Diploma/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Diploma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Diploma',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
