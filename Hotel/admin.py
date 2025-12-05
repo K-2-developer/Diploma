221344fae5cfb1ee7b12dcd9265f4b7693c3031c
Kovalyov <dev.kovalyov@gmail.com>
2025-12-05 16:19:03 +0300

    created admin, add models to admin panel

--- Hotel/admin.py
+++ Hotel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from Hotel.models import Hotel
+from Hotel.models import Room
+
+
+admin.site.register(Hotel)
+admin.site.register(Room)
+
 # Register your models here.
