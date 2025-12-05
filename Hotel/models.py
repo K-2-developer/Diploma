5bb471204e8259d9a7cafeacfd3edcdb504bac62
Kovalyov <dev.kovalyov@gmail.com>
2025-12-05 14:55:21 +0300

    add domain models

--- Hotel/models.py
+++ Hotel/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model):
+    hotel_id = models.AutoField(primary_key=True)
+    hotel_name = models.TextField(max_length=100)
+    hotel_location = models.TextField(max_length=100)
+    hotel_photos = models.TextField(max_length=1000)
+    hotel_rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.hotel_name
+
+
+class Room(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    room_photos = models.TextField(max_length=1000)
+    room_price = models.IntegerField()
+    available = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.room_id
