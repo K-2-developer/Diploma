da7ddf1c535cce10150d8af91f336a9fb5afc8f8
Kovalyov <dev.kovalyov@gmail.com>
2025-12-07 10:53:07 +0300

    added custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    #booking добавить через class Booking и внешний ключ
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
+
+
+
