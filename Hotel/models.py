8e894c6241f169f57dcd27c169f4cd3383bf25ba
Kovalyov <dev.kovalyov@gmail.com>
2025-12-07 12:11:30 +0300

    added booking model

--- Hotel/models.py
+++ Hotel/models.py
@@ -11,7 +11,7 @@ class Hotel(models.Model):
     hotel_rating = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
 
     def __str__(self):
@@ -33,7 +33,7 @@ class Room(models.Model):
     available = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.room_id
@@ -46,3 +46,16 @@ class RoomPhoto(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    chck_out = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.booking_id
