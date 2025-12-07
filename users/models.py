b720301f13be146d7f2f12810245a6f3e0981f65
Kovalyov <dev.kovalyov@gmail.com>
2025-12-07 10:58:16 +0300

    added review model

--- users/models.py
+++ users/models.py
@@ -10,4 +10,14 @@ class User(models.Model):
     updated_at =models.DateTimeField(auto_now=True)
 
 
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
 
+    
