15a139e4b3bf8bfe8c0dee9dd8342f67ead03e7e
Kovalyov <dev.kovalyov@gmail.com>
2025-12-11 10:51:47 +0300

    registered Bookings in admin.py

--- Hotel/admin.py
+++ Hotel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Hotel.models import Hotel, HotelPhoto
+from Hotel.models import Hotel, HotelPhoto, Booking
 from Hotel.models import Room, RoomPhoto
 
 
@@ -7,5 +7,6 @@ admin.site.register(Hotel)
 admin.site.register(Room)
 admin.site.register(HotelPhoto)
 admin.site.register(RoomPhoto)
+admin.site.register(Booking)
 
 # Register your models here.
