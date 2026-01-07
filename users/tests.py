08b91784f2cf3b4349e80dc0e0019dda119870bf
Kovalyov <dev.kovalyov@gmail.com>
2026-01-07 09:26:31 +0300

    added integration test for booking

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from Hotel.models import Hotel,Room,Booking
+from users.models import Review
 
-# Create your tests here.
+class IntegrationTest(TestCase):
+    '''A class for testing integrations'''
+
+    def setUp(self):
+        '''Creating test objects for integration tests'''
+        self.client = Client()
+        self.user = User.objects.create_user(username='test', email='t@gmail.com', password='12345')
+        self.admin = User.objects.create_superuser(username='admin', email='t2@gmail.com', password='11111')
+        self.hotel = Hotel.objects.create(hotel_name="Test Hotel", hotel_location="Belarus", hotel_rating=5)
+        self.room = Room.objects.create(hotel_id=self.hotel, type="standard", room_price=100, available=True)
+
+    def test_booking(self):
+        '''Testing booking functionality'''
+        self.client.login(username='test', password='12345')
+        response = self.client.post(
+            f"/room/{self.room.room_id}/booking/",
+            {"check_in": "2026-01-10",
+             "check_out": "2026-01-15"
+             })
+        self.assertEqual(response.status_code, 302)
+        booking = Booking.objects.get(room_id=self.room, user_id=self.user)
+        self.assertEqual(str(booking.check_in), "2026-01-10")
+        self.assertEqual(str(booking.check_out), "2026-01-15")
+
+    
